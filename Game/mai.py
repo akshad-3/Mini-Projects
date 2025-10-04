97f55aaa0b1b767e7cb1c9bb99b1cbad545d7eed
akshad-3 <akshadofficial3@gmail.com>
2025-10-04 23:16:25 +0530

    Added get_rect method

--- Game/mai.py
+++ Game/mai.py
@@ -9,14 +9,19 @@ test_font=pygame.font.Font('Game/Pixel Game.otf',80)
 
 
 back_surface=pygame.image.load('Game/clouds.jpg').convert()
+
 ground_surface=pygame.image.load('Game/ground.jpg').convert()
+
 text_surfece=test_font.render('First Game',False,'White')
+
 flower_surface=pygame.image.load('Game/flower.png').convert_alpha()
 flower_surface = pygame.transform.scale_by(flower_surface, 0.09)  
-flower_x=730
+flower_rect=flower_surface.get_rect(bottomright=(600,370))
+
 cat_surface=pygame.image.load('Game/cat.png')
 cat_surface=pygame.transform.scale_by(cat_surface,0.09)
-cat_rect=cat_surface.get_rect(topleft=(0,293))
+cat_rect=cat_surface.get_rect(topleft=(0,280))
+
 
 
 
@@ -27,14 +32,15 @@ while True:
             exit()
     
     screen.blit(back_surface,(0,0))
-    screen.blit(ground_surface,(0,370))
+    screen.blit(ground_surface,(0,356))
     screen.blit(text_surfece,(230,50))
-    flower_x-=4
-    if flower_x==-50:flower_x=730
-    screen.blit(flower_surface,(flower_x,293))
-    cat_rect.left+=3
+    
+    flower_rect.x-=4
+    if flower_rect.right<=0:flower_rect.left=736
+    screen.blit(flower_surface,flower_rect)
+    
     screen.blit(cat_surface,cat_rect)
 
     pygame.display.update()
-    clock.tick(70)
+    clock.tick(60)
     
