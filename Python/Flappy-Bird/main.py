792bf80d3435fb2e2b83e21bbacbdce22b6b2745
akshad-3 <akshadofficial3@gmail.com>
2025-10-15 15:52:15 +0530

    final game

--- Python/Flappy-Bird/main.py
+++ Python/Flappy-Bird/main.py
@@ -22,6 +22,7 @@ def welcomescreen():
                 screen.blit(game_item['player'],(playerx,playery))
                 screen.blit(game_item['base'],(basex,540))
                 screen.blit(game_item['message'],(messagex,messagey))
+                #game_sound['menu'].play()
                 pygame.display.update()
                 clock.tick(60)
 def maingame():
@@ -35,11 +36,11 @@ def maingame():
     
     upperpipe=[
         {'x':SCREENWIDTH+200,'y':newpipe1[0]['y']},
-        {'x':SCREENWIDTH+200+(SCREENWIDTH/2),'y':newpipe1[0]['y']}
+        {'x':SCREENWIDTH+200+(SCREENWIDTH/2),'y':newpipe2[0]['y']}
     ]
     lowerpipe=[
         {'x':SCREENWIDTH+200,'y':newpipe1[1]['y']},
-        {'x':SCREENWIDTH+200+(SCREENWIDTH/2),'y':newpipe1[1]['y']}
+        {'x':SCREENWIDTH+200+(SCREENWIDTH/2),'y':newpipe2[1]['y']}
     ]
     
     pipespeedx=-4
@@ -63,8 +64,7 @@ def maingame():
                 if playery > 0:
                     playerspeedY = playerFLAPING
                     playerFLPPED=True
-                    #gamesound
-                
+                    game_sound['wing'].play()               
         crashing= isCollide(playerx,playery,upperpipe,lowerpipe)
 
         if crashing:
@@ -76,6 +76,7 @@ def maingame():
             if pipeMID <= playerMID < pipeMID + 4:
                 score+=1
                 print(f"your score is {score}")
+                game_sound['point'].play()
             #gamesound
         
         if playerspeedY < playerMAXspeedY and not playerFLPPED:
@@ -107,24 +108,65 @@ def maingame():
         
         screen.blit(game_item['base'],(basex,540))
         screen.blit(game_item['player'],(playerx,playery))
+        myDigits = [int(x) for x in list(str(score))]
+        width = 0
+        for digit in myDigits:
+            width += game_item['numbers'][digit].get_width()
+        offset = (SCREENWIDTH - width)/2
+
+        for digit in myDigits:
+            screen.blit(game_item['numbers'][digit], (offset, SCREENHIGHT*0.12))
+            offset += game_item['numbers'][digit].get_width()
         
         pygame.display.update()
         clock.tick(60)
             
 
 def isCollide(playerx,playery,upperpipe,lowerpipe):
+    if playery > 540-65 or playery<0:
+        game_sound['die'].play()
+        return True
+    
+    for pipe in upperpipe:
+        pipeHight=game_item['pipe'][0].get_height()
+        if (playery < pipeHight + pipe['y'] and abs(playerx - pipe['x']) < game_item['pipe'][0].get_width()):
+            game_sound['die'].play()
+            return True
+    
+    for pipe in lowerpipe:
+        if (playery + game_item['player'].get_height() > pipe['y']) and abs(playerx - pipe['x']) < game_item['pipe'][0].get_width():
+            game_sound['die'].play()
+            return True
     return False
+# def getrandomepipe():
+#     pipeHight=game_item['pipe'][0].get_height()
+#     offset = SCREENHIGHT/3
+#     y2 = offset + random.randrange(0,int(SCREENHIGHT - game_item['base'].get_height() - 1.2 * offset))
+#     pipex=SCREENWIDTH + 10
+#     y1 = pipeHight - y2 + offset
+#     pipe=[
+#         {'x': pipex,'y': y1},
+#         {'x':pipex,'y':y2}
+#     ]
+#     return pipe
+
 def getrandomepipe():
-    pipeHight=game_item['pipe'][0].get_height()
-    offset = SCREENHIGHT/3
-    y2 = offset + random.randrange(0,int(SCREENHIGHT - game_item['base'].get_height() - 1.2 * offset))
-    pipex=SCREENWIDTH + 10
-    y1 = pipeHight - y2 + offset
-    pipe=[
-        {'x': pipex,'y': y1},
-        {'x':pipex,'y':y2}
+    offset = SCREENHIGHT / 3  
+    pipe_height = game_item['pipe'][0].get_height()
+    base_height = game_item['base'].get_height()
+
+    y2 = offset + random.randrange(0, int(SCREENHIGHT - base_height - 1.5 * offset))
+    pipex = SCREENWIDTH + 10
+
+   
+    y1 = y2 - pipe_height - offset
+
+    pipe = [
+        {'x': pipex, 'y': y1},
+        {'x': pipex, 'y': y2}   
     ]
     return pipe
+
 SCREENWIDTH=801
 SCREENHIGHT=603     
 # GROUNDY=SCREENHIGHT * 0.8
@@ -144,6 +186,19 @@ test_font=pygame.font.Font('font.otf',70)
 # game_item['number']=(
 #     pygame.image.load('')
 # )
+game_item['numbers']=(
+    pygame.transform.scale_by(pygame.image.load('0.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('1.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('2.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('3.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('4.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('5.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('6.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('7.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('8.png').convert_alpha(),0.5),
+    pygame.transform.scale_by(pygame.image.load('9.png').convert_alpha(),0.5)
+    
+)
 game_item['message']=pygame.image.load('message.png').convert_alpha()
 game_item['base']=pygame.image.load('ground.png').convert_alpha()
 game_item['pipe']=(
@@ -152,8 +207,12 @@ game_item['pipe']=(
 )
 
 game_item['background']=pygame.image.load(background).convert()
-game_item['player']=pygame.transform.scale_by(pygame.image.load(player).convert_alpha(),0.2)
+game_item['player']=pygame.transform.scale_by(pygame.image.load(player).convert_alpha(),0.3)
 
+game_sound['die'] = pygame.mixer.Sound('dry-fart.mp3')
+game_sound['point'] = pygame.mixer.Sound('point.wav')
+game_sound['wing'] = pygame.mixer.Sound('wing.mp3')
+game_sound['menu'] = pygame.mixer.Sound('menu.mp3')
 
 while True:
     welcomescreen()
