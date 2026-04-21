7a7befe3691f2c60324003faa44a53d07091e1d8
akshad-3 <akshadofficial3@gmail.com>
2026-04-22 03:12:18 +0530

    changes made in app.py

--- Python/cifake-backend/app.py
+++ Python/cifake-backend/app.py
@@ -50,4 +50,6 @@ def predict():
     return jsonify({"result": result, "confidence": confidence})
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    #app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
