4bed73b179186d30b60005d737c4014177a2535a
akshad-3 <akshadofficial3@gmail.com>
2026-06-28 00:43:01 +0530

    made a working and runing server

--- Full-Stack/Authentication-API/index.js
+++ Full-Stack/Authentication-API/index.js
@@ -1,2 +1,10 @@
 import dotenv from "dotenv";
-dotenv.config();
+import app from "./app.js"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT,()=>{
+    console.log(`server is running on the port http://localhost:${PORT || 8000}`);   
+})
